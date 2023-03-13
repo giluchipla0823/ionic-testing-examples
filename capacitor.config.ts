36fc0b70119352c3b045af3939ec653ad0963264
Luiggi Plasencia <luiggi.plasencia@uvesolutions.com>
2023-03-13 20:18:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-testing-examples',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
